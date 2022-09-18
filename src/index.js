b38b49921e8c2d51eeca70849edf2dc918b5b7e7
songsong <jws_17628484913@163.com>
2022-09-18 11:07:31 +0800

    模版标记

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const demo = document.querySelector('.demo');
+
+demo.addEventListener('click',() => {
+    
+})
+
+demo.setStyles`
+        backgroundColor: red;
+        fontSize: ${20}px;
+        height: ${100}px;
+`.setContent`加油`
