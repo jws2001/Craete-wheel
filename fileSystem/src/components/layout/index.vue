45cbcae3f97c6361fc35598e84dc14ad606a48f6
jws2001 <jws_17628484913@163.com>
2023-02-25 12:48:07 +0800

    布局组件

---
+++ fileSystem/src/components/layout/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="layout-container">
+        <div class="menu">菜单栏</div>
+        <div class="content">
+            <aside id="aside">
+                <slot name="aside"></slot>
+            </aside>
+            <main id="main">
+                <slot></slot>
+            </main> 
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.layout-container{
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgb(30, 30, 30);
+    .menu{
+        width: 50px;
+        border: 1px solid red;
+        flex-grow: 0;
+        flex-shrink: 0;
+    }
+}
+</style>
