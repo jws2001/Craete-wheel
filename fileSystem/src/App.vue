318c577abbeff1729c2852400cd236ea93803794
jws2001 <jws_17628484913@163.com>
2023-02-25 12:47:51 +0800

    项目初始化

---
+++ fileSystem/src/App.vue
@@ -0,0 +1,38 @@
+
+
+<template>
+    <Layout />
+</template>
+
+<script setup lang="ts">
+import Layout from './components/layout/index.vue';
+// 处理目录
+async function processHandle(handle: any) {
+    if (handle.kind === 'file') {
+        // 文件直接返回
+        return handle;
+    }
+    // 是目录
+    handle.children = [];
+
+    // 得到一个 异步迭代器
+    const iter = handle.entries();
+    // 循环得到文件里面的内容
+    for await (const item of iter) {
+        handle.children.push(await processHandle(item[1]))
+    }
+    return handle;
+}
+const handleClick = async () => {
+    try {
+        // showDirectoryPicker
+        const directory = await window.showDirectoryPicker();
+        const root = await processHandle(directory);
+        console.log(root)
+    } catch (err) {
+
+    }
+}
+</script>
+
+<style scoped></style>
---
+++ fileSystem/src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './reset.scss'
+import './style.scss'
+
+createApp(App).mount('#app')
