b68a754e9277d40e09d414acef3257cfcd5e2da3
jws2001 <jws_17628484913@163.com>
2023-03-16 21:29:08 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-function delay (time) {
-    return new Promise((resolve, reject) => {
+function delay(time) {
+    return new Promise(resolve => {
         setTimeout(() => resolve(), time)
     })
 }
 
 class SuerTask {
-    constructor(count = 2){
+    constructor(count = 2) {
         this.count = count; // 最大并发数
         this.runingCount = 0; // 当前正在运行的数量
         this.tasks = [];
     }
-    add(task){
+    add(task) {
         return new Promise((resolve, reject) => {
             this.tasks.push({
                 task,
@@ -20,12 +20,12 @@ class SuerTask {
             this.#run();
         })
     }
-    #run(){
-        while(this.tasks.length && this.runingCount < this.count){
+    #run() {
+        while (this.tasks.length && this.runingCount < this.count) {
             const { task, resolve, reject } = this.tasks.shift();
-            this.runingCount ++;
+            this.runingCount++;
             task().then(resolve, reject).finally(() => {
-                this.runingCount --;
+                this.runingCount--;
                 this.#run();
             })
         }
@@ -34,12 +34,12 @@ class SuerTask {
 
 const suerTask = new SuerTask();
 
-function addTask(time, name){
+function addTask(time, name) {
     suerTask
-    .add(() => delay(time))
-    .then(() => {
-        console.log(`任务${name}完成`)
-    })
+        .add(() => delay(time))
+        .then(() => {
+            console.log(`任务${name}完成`)
+        })
 }
 
 
