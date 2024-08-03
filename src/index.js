691f0da6f875e77ab9647862fef12f6d2a1a597f
焦文松 <jiaowensong@xiaoduotech.com>
2024-08-03 17:53:10 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,35 +1,47 @@
 
-const moveCursorToEnd = (element) => {
-    const range = document.createRange();
-    const selection = window.getSelection();
-    range.selectNodeContents(element);
-    range.collapse(false);
-    selection.removeAllRanges();
-    selection.addRange(range);
-  };
 
+const len = document.querySelector('.len')
 const editable = document.querySelector('#editable');
+const maxLength = 10;
+let isComposing = false;
 
-editable.addEventListener('paste', function (event) {
-    event.preventDefault();
-    const clipboardData = event.clipboardData || window.clipboardData;
-    const items = clipboardData.items;
 
-    for (let i = 0; i < items.length; i++) {
-      if (items[i].type.indexOf('image') !== -1) {
-        const file = items[i].getAsFile();
-        const reader = new FileReader();
-        reader.onload = function (event) {
-          const img = document.createElement('img');
-          img.src = event.target.result;
-          editable.appendChild(img);
-          moveCursorToEnd(editable);
-        };
-        reader.readAsDataURL(file);
-      } else {
-        // For other types of content (e.g., text), you can handle them here
-        const text = clipboardData.getData('text/plain');
-        document.execCommand('insertText', false, text);
-      }
-    }
-  });
+function truncateText() {
+  let text = editable.innerText.replace(/\n/g, ''); // 去掉所有的换行符
+  if (text.length > maxLength) {
+    editable.innerText = text.substring(0, maxLength);
+    moveCursorToEnd(editable);
+  }
+  setLen();
+}
+
+editable.addEventListener('input', function () {
+  if (!isComposing) {
+    truncateText();
+  }
+});
+
+editable.addEventListener('compositionstart', function () {
+  isComposing = true;
+});
+
+editable.addEventListener('compositionend', function () {
+  isComposing = false;
+  truncateText();
+});
+
+function setLen() {
+  len.innerText = `当前输入长度：${editable.innerText.length}`;
+}
+
+setLen()
+
+
+function moveCursorToEnd(element) {
+  const range = document.createRange();
+  const selection = window.getSelection();
+  range.selectNodeContents(element);
+  range.collapse(false);
+  selection.removeAllRanges();
+  selection.addRange(range);
+};
